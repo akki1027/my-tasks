4363e1c78f4230fd7ad4a5da369ced84e7432fb0
akki1027 <akki10271995@gmail.com>
2021-11-04 07:26:26 +0900

    Tailwind cssの導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,31 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <button className="btn btn-green hover:bg-yellow-200">hello from tailwind</button>
+
+        <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+          <div className="text-center space-y-2 sm:text-left">
+            <div className="space-y-0.5">
+              <p className="text-lg text-black font-semibold">
+                Erin Lindford
+              </p>
+              <p className="text-gray-500 font-medium">
+                Product Engineer
+              </p>
+            </div>
+            <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+          </div>
+        </div>
+
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+          <div className="flex-shrink-0">
+          </div>
+          <div>
+            <div className="text-xl font-medium text-black">ChitChat</div>
+            <p className="text-gray-500">You have a new message!</p>
+          </div>
+        </div>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
